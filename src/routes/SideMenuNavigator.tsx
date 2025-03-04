ce8cad02871bfbb8b50a8b3151bd1c7e35c75fd7
pedro <elprogramadorpedro@gmail.com>
2025-03-04 18:48:19 -0300

    custom drawer

--- src/routes/SideMenuNavigator.tsx
+++ src/routes/SideMenuNavigator.tsx
@@ -1,14 +1,62 @@
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { StackNavigator } from './StackNavigator';
-import { ProfileScreen } from '../screens/profile/ProfileScreen';
+import {
+    createDrawerNavigator,
+    DrawerContentComponentProps,
+    DrawerContentScrollView,
+    DrawerItemList,
+  } from '@react-navigation/drawer';
+  import {StackNavigator} from './StackNavigator';
+  import {ProfileScreen} from '../screens/profile/ProfileScreen';
+  import {globalColors} from '../theme/theme';
+  import {View, Text, useWindowDimensions} from 'react-native'; // Cambiado a react-native
 
-const Drawer = createDrawerNavigator();
+  
+  const Drawer = createDrawerNavigator();
 
-export const SideMenuNavigator = () => {
-  return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="StackNavigator" component={StackNavigator} />
-      <Drawer.Screen name="Profile" component={ProfileScreen} />
-    </Drawer.Navigator>
-  )
-}
+  const dimensions = useWindowDimensions();
+  
+  export const SideMenuNavigator = () => {
+    return (
+      <Drawer.Navigator
+      
+        
+        // personalizando aún más el drawer
+        drawerContent={props => <CustomDrawerContent {...props} />}
+        // me permite personalizar el drawer screenOptions
+        screenOptions={{
+
+          drawerType: (dimensions.width>=758) ? 'permanent':'slide',  
+          headerShown: false,
+          
+  
+          drawerActiveBackgroundColor: globalColors.primary, // fondo color del drawer
+          drawerActiveTintColor: 'white', // tipografía color del drawer
+  
+          drawerItemStyle: {
+            borderRadius: 100,
+            paddingHorizontal: 20,
+          },
+        }}>
+        <Drawer.Screen name="StackNavigator" component={StackNavigator} />
+        <Drawer.Screen name="Profile" component={ProfileScreen} />
+      </Drawer.Navigator>
+    );
+  };
+  
+  const CustomDrawerContent = (props: DrawerContentComponentProps) => {
+    return (
+      <DrawerContentScrollView {...props}>
+        <View
+          style={{
+            height: 200,
+            backgroundColor: globalColors.primary,
+            margin: 30,
+            borderRadius: 50,
+          }}></View>
+
+
+          <DrawerItemList {...props}/> 
+
+          <Text>Texto complementarr</Text>
+      </DrawerContentScrollView>
+    );
+  };

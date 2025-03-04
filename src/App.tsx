eb617f9204a0cf14cc3a3e83761896dd78fe4b30
pedro <elprogramadorpedro@gmail.com>
2025-03-04 16:03:01 -0300

    drawer intallation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import * as React from 'react';
 
 
-import { StackNavigator } from './routes/StackNavigator';
+//import { StackNavigator } from './routes/StackNavigator';
+import { SideMenuNavigator } from './routes/SideMenuNavigator';
 
 export const App = () => {
   return (
 <NavigationContainer>
-  <StackNavigator></StackNavigator>
+  {/*<StackNavigator></StackNavigator>*/}   
+  <SideMenuNavigator></SideMenuNavigator>
 </NavigationContainer>
   )
 }
